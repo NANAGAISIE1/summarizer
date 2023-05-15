468aa64cf70907aa522224ea834c44f1c5f8bc24
Nana Gaisie <nabgaisie@hotmail.com>
2023-05-15 14:23:27 +0000

    Vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { inject } from '@vercel/analytics';
 
 import App from "./App";
 import { store } from "./services/store";
+
+ 
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
